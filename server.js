0fd0e705a07a1e5204159a45363c74a6244dfd90
nlilu <nlilum.kim@gmail.com>
2021-05-15 17:46:55 -0400

    cleaned up code to connect to sql database

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 //connection to the database
-const db = require('./db/connection');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Summer12@',
+    database: 'tracker'
+});
+//Connects to sql server and database
+connection.connect(function(err){
+    if (err) throw err; 
+    options();
+});
+
 
 
 
