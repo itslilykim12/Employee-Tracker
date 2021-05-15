bf05b170c6a70c5119888699722ffe3bb6da8bc8
nlilu <nlilum.kim@gmail.com>
2021-05-15 19:41:05 -0400

    added user prompts for server.js

--- server.js
+++ server.js
@@ -15,6 +15,47 @@ connection.connect(function(err){
     options();
 });
 
-
+//prompts users with the lists of options to choose from 
+function options() { 
+    inquirer.prompt({
+        name: 'action',
+        type: ' list',
+        message: 'Welcome to our employee tracker database! What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+            'Add an employee',
+            'Add a department',
+            'Add a role',
+            'Update an employee role',
+            'Delete an employee',
+            'EXIT/QUIT'
+        ]
+    }).then(function(answer) {
+        switch (answer.action) {
+        case 'View all employees': viewEmployees();
+        break;
+        case 'View all departments': viewDepartments();
+        break;
+        case 'View all roles': viewRoles();
+        break;
+        case 'Add an employee': addEmployee();
+        break;
+        case 'Add a department': addDepartment();
+        break;
+        case 'Add a role': addRole();
+        break;
+        case 'Update an employee role': updateRole();
+        break;
+        case 'Delete an employee': deleteEmployee();
+        break;
+        case 'EXIT/QUIT': exitApp();
+        break;
+        default:
+            break;
+        }
+    })
+};
 
 
