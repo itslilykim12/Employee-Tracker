317485189d5fda48871c936edaff4d63aef3117d
nlilu <nlilum.kim@gmail.com>
2021-05-15 21:46:40 -0400

    cleaned up sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,6 @@
-DROP DATABASE IF EXISTS tracker;
-
-CREATE DATABASE tracker; 
-
-USE tracker;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
@@ -11,21 +9,19 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-     id INT NOT NULL AUTO_INCREMENT,
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
      title VARCHAR(30) NOT NULL,
      salary DECIMAL NOT NULL,
-     department_id INT NOT NULL,
-     PRIMARY KEY(id),
-     FOREIGN KEY (deparment_id) REFERENCES department(id)
+     department_id INTEGER,
+     CONSTRAINT fk _department FOREIGN KEY (deparment_id) REFERENCES department(id) ON DELETE CASCADE
      );
 
      CREATE TABLE employee (
-     id INT NOT NULL AUTO_INCREMENT,
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
      first_name VARCHAR(30) NOT NULL,
      last_name VARCHAR(30) NOT NULL,
-     role_id INT NOT NULL,
-     manager_id INT NULL,
-     PRIMARY KEY(id),
-     FOREIGN KEY(role_id) REFERENCES role(id),
-     FOREIGN KEY(manager_id) REFERENCES employee(id)
+     role_id INTEGER,
+     manager_id INTEGER,
+     CONSTRAINT fk_ role FOREIGN KEY(role_id) REFERENCES role(id),
+     CONSTRAINT fk _manager FOREIGN KEY(manager_id) REFERENCES employee(id)
     );
